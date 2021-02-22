848a7ee4ed1a8fabe143b50c817886dea3ee6d6a
Tommy Harris <tobraha@tilde.team>
2021-02-22 13:45:42 -0700

    Add export for freerdp2 monitor string on laptop

--- scripts/shell/exports.sh
+++ scripts/shell/exports.sh
@@ -7,3 +7,9 @@ export TZ="America/Denver"
 # define 'less'; only use pagination if output exceeds terminal
 export LESS="-F -X $LESS"
 
+# set monitor numbers for use with freerdp2
+
+monitors=$(xfreerdp /monitor-list | tr "[*]" ' ')
+mon1=$(echo $monitors | grep '+3840' | awk ' { print $1 } ')
+mon2=$(echo $monitors | grep '+1920' | awk ' { print $1 } ')
+export RDP_MONS="$mon2,$mon1"
