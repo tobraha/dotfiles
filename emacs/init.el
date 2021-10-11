4b0e4d7080819c5313d72b4562f885cb470ca09b
Tommy Harris <tobraha@tilde.team>
2021-10-11 16:02:46 -0600

    Removing spacemacs: continue setup

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,38 @@
 ;; basics setup
-;; 
+;; -> setup packages
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'use-package)
+(setq use-package-alwasy-ensure t)
+
+;; -> custom options
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default))
+ '(electric-pair-mode t)
+ '(org-agenda-files
+   '("c:/Users/thomas.harris/OneDrive - Insurity, Inc/tommy_notebook.org.gpg"))
+ '(package-selected-packages
+   '(org-bullets window-numbering use-package swiper multiple-cursors gruvbox-theme expand-region))
+ '(window-numbering-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; use emacs for gpg pin entry
 (setq epa-pinentry-mode 'loopback)
@@ -13,6 +46,12 @@
 (tooltip-mode -1)
 (set-fringe-mode 10)
 (menu-bar-mode -1)
+
+;; buffer switching
+(setq-default minor-mode 'window-numberingmode)
+
+;; default electric pair mode
+(setq-default minor-mode 'electric-pair-mode)
 
 ;; ========== Multiple Cursors =============================
 (require 'multiple-cursors)
@@ -42,29 +81,42 @@
 ;; use "C-;" for er/expand-region
 (global-set-key (kbd "C-;") 'er/expand-region)
 
-;; org mode shortcuts
+;; org mode shortcuts and apps
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
+(setq org-file-apps
+      (quote
+       ((auto-mode . emacs)
+        ("\\.mm\\'" . default)
+        ("\\.x?html?\\'" . "C:\\Users\\thomas.harris\\AppData\\Local\\qutebrowser\\qutebrowser.exe %s")
+        ("\\.pdf\\'" . default))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red3" :weight bold)
+              ("IN-PROGRESS" :foreground "green" :weight bold)
+              ("WAITING" :foreground "green" :weight bold)
+              ("SCHEDULED" :foreground "orange" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold))))
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; use emacs for gpg pin entry, not local gpg agent
+(setq epa-pinentry-mode 'loopback)
+
+;; follow symlinks by default
+(setq vc-follow-symlinks t)
+
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Iosevka")
+(set-face-attribute 'default nil :font "Iosevka Extended"
+		    :height 90
+		    :weight 'normal
+		    :width 'normal)
 
-(load-theme 'gruvbox-dark-medium)
-
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(require 'use-package)
-(setq use-package-alwasy-ensure t)
+(load-theme 'gruvbox-dark-medium t)
 
 ;; Use C-c a to open org agenda
 (add-hook 'org-mode-hook
